b97ded77c7314edee4a0a25f4526367342ecccce
multifacs <multifacs@users.noreply.github.com>
2020-05-19 22:28:37 +0300

    one more thing

--- GeometricShapes/main.cpp
+++ GeometricShapes/main.cpp
@@ -8,42 +8,50 @@
 #include "Container.h"
 
 #define Add(x) Add(x, #x)
-#define SetA(t) SetA()[t]
-#define SetB(t) SetB()[t]
-#define SetC(t) SetC()[t]
-#define SetD(t) SetD()[t]
 
 using namespace std;
 
 int main()
 {
 	Point<int> Dot(2);
-	Dot.SetA(0) = 1; Dot.SetA(1) = 2;
+	Dot.SetA()[0] = 1;
+	Dot.SetA()[1] = 2;
 	cout << Dot << endl;
 	Point<int> Dot2(Dot);
 
 	Line<int> Vec(2);
-	Vec.SetA(0) = 0; Vec.SetA(1) = 0;
-	Vec.SetB(0) = 3; Vec.SetB(1) = -4;
+	Vec.SetA()[0] = 0;
+	Vec.SetA()[1] = 0;
+	Vec.SetB()[0] = 3;
+	Vec.SetB()[1] = -4;
 	cout << Vec << endl;
 	Line<int> Vec2(Vec);
 
 	Circle<int> Cc(2);
-	Cc.SetA(0) = 0; Cc.SetA(1) = 0;
-	Cc.SetB(0) = 3; Cc.SetB(1) = -4;
+	Cc.SetA()[0] = 0;
+	Cc.SetA()[1] = 0;
+	Cc.SetB()[0] = 3;
+	Cc.SetB()[1] = -4;
 	cout << Cc << endl;
 
 	Triangle<int> Tri(2);
-	Tri.SetA(0) = 0; Tri.SetA(1) = 0;
-	Tri.SetB(0) = 0; Tri.SetB(1) = 2;
-	Tri.SetC(0) = 3; Tri.SetC(1) = 0;
+	Tri.SetA()[0] = 0;
+	Tri.SetA()[1] = 0;
+	Tri.SetB()[0] = 0;
+	Tri.SetB()[1] = 2;
+	Tri.SetC()[0] = 3;
+	Tri.SetC()[1] = 0;
 	cout << Tri << endl;
 
 	Square<int> Sq(2);
-	Sq.SetA(0) = 0; Sq.SetA(1) = 0;
-	Sq.SetB(0) = 0; Sq.SetB(1) = 2;
-	Sq.SetC(0) = 2; Sq.SetC(1) = 2;
-	Sq.SetD(0) = 2; Sq.SetD(1) = 0;
+	Sq.SetA()[0] = 0;
+	Sq.SetA()[1] = 0;
+	Sq.SetB()[0] = 0;
+	Sq.SetB()[1] = 2;
+	Sq.SetC()[0] = 2;
+	Sq.SetC()[1] = 2;
+	Sq.SetD()[0] = 2;
+	Sq.SetD()[1] = 0;
 	cout << Sq << endl;
 	Square<int> Sk(Sq);
 
@@ -61,14 +69,24 @@ int main()
 	cout << endl;
 
 	Sphere<int> Sph(3);
-	Sph.SetA(0) = 0; Sph.SetA(1) = 0; Sph.SetA(2) = 0;
-	Sph.SetB(0) = 5; Sph.SetB(1) = 5; Sph.SetB(2) = 0;
+	Sph.SetA()[0] = 0;
+	Sph.SetA()[1] = 0;
+	Sph.SetA()[2] = 0;
+	Sph.SetB()[0] = 5;
+	Sph.SetB()[1] = 5;
+	Sph.SetB()[2] = 0;
 	cout << Sph << endl;
 
 	Cone<int> El(3);
-	El.SetA(0) = 2; El.SetA(1) = 2; El.SetA(2) = 0;
-	El.SetB(0) = 0; El.SetB(1) = 0; El.SetB(2) = 5;
-	El.SetC(0) = 0; El.SetC(1) = 0; El.SetC(2) = 0;
+	El.SetA()[0] = 2;
+	El.SetA()[1] = 2;
+	El.SetA()[2] = 0;
+	El.SetB()[0] = 0;
+	El.SetB()[1] = 0;
+	El.SetB()[2] = 5;
+	El.SetC()[0] = 0;
+	El.SetC()[1] = 0;
+	El.SetB()[2] = 0;
 	cout << El << endl;
 
 	return 0;
